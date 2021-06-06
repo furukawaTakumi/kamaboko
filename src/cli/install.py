b2e07ebd890fc2720c9ac4f4aa003ac2f178662a
furukawaTakumi <black3moon88@gmail.com>
2021-06-07 08:54:56 +0900

    [change]jchanged dictionary structure to a structure that can be used for both collocations and vocabularies. and support for various file structure.

--- src/cli/install.py
+++ src/cli/install.py
@@ -11,7 +11,7 @@ def install():
 
     with open(args.dic_path, 'r') as f:
         data = csv.reader(f, delimiter=__delimiter(args.file_format))
-        dictionary = __construct_dict(data)
+        dictionary = __construct_dict(data, args)
 
     save_dir = f"{os.path.dirname(kamaboko.__file__)}/resource/{args.dic_type}"
     filename = args.dic_path.split('/')[-1] + datetime.datetime.now().strftime('%Y__%m__%d-%H__%M__%S.json')
@@ -33,15 +33,42 @@ def __parse_args():
     parser.add_argument(
         'dic_type',
         help='type of dictionary',
-        metavar='noun/collocation',
         choices=['noun', 'collocation']
     )
     parser.add_argument(
         '--file_format',
+        '-ff',
         help='file type',
-        metavar='csv/tsv',
         default='csv',
         choices=['csv', 'tsv'],
+    )
+    parser.add_argument(
+        '--word_idx',
+        '-wi',
+        help='index of word column',
+        default=0,
+        type=int
+    )
+    parser.add_argument(
+        '--polality_idx',
+        '-pi',
+        help='index of polality column',
+        default=1,
+        type=int
+    )
+    parser.add_argument(
+        '--positive_labels',
+        '-pl',
+        action='append',
+        help='positive labels',
+        default=['p'],
+    )
+    parser.add_argument(
+        '--negative_labels',
+        '-nl',
+        action='append',
+        help='negative labels',
+        default=['n'],
     )
 
     return parser.parse_args()
@@ -56,8 +83,27 @@ def __delimiter(fmt_str: str):
         'tsv': '\t'
     }[fmt_str]
 
-def __construct_dict(data: Iterator) -> dict:
+def __construct_dict(data: Iterator, args) -> dict:
     dictionary = {}
     for row in data:
-        dictionary[row[0]] = row[1]
+        keys = row[args.word_idx].split(' ')
+        tmp_dict = dictionary
+        for idx in range(0, len(keys)):
+            tmp_dict[keys[idx]] = {
+                'dic_type': args.dic_type,
+                'is_end': False
+            }
+            if idx == len(keys) - 1:
+                tmp_dict[keys[idx]]['is_end'] = True
+                tmp_dict[keys[idx]]['polality'] = __polality_label(row[args.polality_idx], args)
+            tmp_dict = tmp_dict[keys[idx]]
     return dictionary
+
+def __polality_label(label, args):
+    if label in args.positive_labels:
+        return 'p'
+    elif label in args.negative_labels:
+        return 'n'
+    else:
+        print(f"Convert Warning: neither polality label '{label}' is not contained '{args.positive_labels}' or '{args.negative_labels}'.")
+        return 'e'
