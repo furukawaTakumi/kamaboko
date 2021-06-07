d2d8238ece106b177e9720e1ff9e27d04818aa0b
furukawaTakumi <black3moon88@gmail.com>
2021-06-07 21:58:08 +0900

    [add]analyze by count polality word.

--- src/kamaboko/kamaboko.py
+++ src/kamaboko/kamaboko.py
@@ -10,11 +10,37 @@ from .PolalityDict import PolalityDict
 class Kamaboko:
     def __init__(self) -> None:
         self.dictionary = PolalityDict()
-        self.tokenizer = MeCab.Tagger().parse
+        self.tokenizer = MeCab.Tagger().parseToNode
 
     def analyze(self, text):
-        tokens = self.tokenizer(text)
-        print('tokens', tokens)
-        for t in tokens:
-            print('t', t)
-        return
+        node = self.tokenizer(text)
+        tokens = self.__conv_to_standard_form(node)
+        result = self.__count_polality(tokens)
+        return result
+
+    def __count_polality(self, tokens: list):
+        positive_num, negative_num = 0, 0
+        tmp_dict = self.dictionary
+        for tkn in tokens:
+            if not tkn in tmp_dict.keys():
+                tmp_dict = self.dictionary
+                continue
+
+            if tmp_dict[tkn]['is_end']:
+                positive_num += tmp_dict[tkn]['polality'] == 'p'
+                negative_num += tmp_dict[tkn]['polality'] == 'n'
+                tmp_dict = self.dictionary
+            else:
+                tmp_dict = self.dictionary[tkn]
+        return positive_num, negative_num
+    
+    def __conv_to_standard_form(self, node):
+        tokens = []
+        while node:
+            standard_form = node.feature.split(',')[6] # standard form exists on index 6
+            tokens.append(standard_form)
+            node = node.next
+        return tokens[1:-1] # token of index 0 and -1 is '*'
+
+
+
--- test/kamaboko_test.py
+++ test/kamaboko_test.py
@@ -6,13 +6,18 @@ from  collections.abc import Mapping
 
 from kamaboko import Kamaboko
 
+# 日本語評価極性辞書（用言編）・日本語評価極性辞書（名詞編）を用いた場合のテスト
+
 class KamabokoTest(unittest.TestCase):
     def setUp(self):
+        self.kamaboko = Kamaboko()
         pass
 
-    def test_create_instance(self):
-        kamaboko = Kamaboko()
-        self.assertIsInstance(kamaboko, Kamaboko)
+    def test_analyze(self):
+        result = self.kamaboko.analyze("""
+        日本にいた頃は一日中、毎晩深夜遅くまで働いていたせいで、美容とかおしゃれとかに縁遠く、立派な喪女だったけど、あまりの変化に最近では鏡を見るのがちょっと楽しい。
+        """)
+        self.assertEqual((3,1), result)
 
     def tearDown(self):
         pass
